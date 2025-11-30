99fa70b9b52bc2339d0621988628cef49571ea49
khaled asfour <khaled.asfour@student.kdg.be>
2025-11-30 22:08:25 +0100

    Added SAC baseline with 3 trials and TensorBoard logs.

--- src/train_baseline.py
+++ src/train_baseline.py
@@ -7,14 +7,18 @@ import os
 
 def train_baseline(cfg_path="config/config_baseline.yaml"):
     cfg = load_config(cfg_path)
-    for trial in cfg['num_trials']:
-        env =  # Create environment
-        algo =  # Pick Algorithm
-        model = algo("MlpPolicy", env, verbose=1, tensorboard_log=f"logs/baseline/{cfg['algorithm']}_trial{trial}/")
+    for trial in range(cfg['num_trials']):
+        env =  gym.make(cfg['environment'])# Create environment
+        algo =  SAC # Pick Algorithm
+
+        model = algo("MlpPolicy", env, verbose=1,
+                     tensorboard_log=f"logs/baseline/{cfg['algorithm']}_trial{trial}/")
+
+        os.makedirs(f"logs/baseline/{cfg['algorithm']}_trial{trial}/checkpoints/",
+                    exist_ok=True)
         cb = CheckpointCallback(save_freq=cfg["checkpoint_freq"],
                                 save_path=f"logs/baseline/{cfg['algorithm']}_trial{trial}/checkpoints/")
         model.learn(total_timesteps=cfg["timesteps"], callback=cb)
-        model.save(f"results/baseline/{cfg['algorithm']}_trial{1}")
-
+        model.save(f"results/baseline/{cfg['algorithm']}/trial{trial}")
 
 if __name__ == '__main__': train_baseline()
